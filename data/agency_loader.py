be4771aaeffd97bf698768d5ae6e6fdf259604a3
jlivni <jlivni@ucs1.umbrellaconsulting.com>
2009-11-17 11:19:47 -0800

    typos in loader

--- data/agency_loader.py
+++ data/agency_loader.py
@@ -1,31 +1,31 @@
 import datetime
 from google.appengine.ext import db
 from google.appengine.tools import bulkloader
-from ..utils.slug import slugify
+from django.template.defaultfilters import slugify
 
 class AgencyLoader(bulkloader.Loader):
     def __init__(self):
     
-    def lat_lon(s):
-        lat, lon = [float(v) for v in s.split(',')]
-        return db.GeoPt(lat, lon)
-    
+        def lat_lon(s):
+            lat, lon = [float(v) for v in s.split(',')]
+            return db.GeoPt(lat, lon)
+        
         bulkloader.Loader.__init__(self, 'Agency',
-                                   [('long_name', str),
-                                    ('short_name', str),
-                                    ('area', str),
-                                    ('state', str),
-                                    ('contact', str),
-                                    ('url', str),
-                                    ('phone', str),
-                                    ('address', str),
-                                    ('location', lat_lon)
-                                   ])
-                                   
-                                   
-    def handle_entity(self, entity):
-        entity.update_location()
-        entity.urlslug = '/'.join(map(slugify,[entity.state, entity.city, entity.name)])
-        return entity
+                                       [('long_name', str),
+                                        ('short_name', str),
+                                        ('area', str),
+                                        ('state', str),
+                                        ('contact', str),
+                                        ('url', str),
+                                        ('phone', str),
+                                        ('address', str),
+                                        ('location', lat_lon)
+                                       ])
+                                       
+                                       
+        def handle_entity(self, entity):
+            entity.update_location()
+            entity.urlslug = '/'.join(map(slugify,[entity.state, entity.city, entity.name]))
+            return entity
 
 loaders = [AgencyLoader]
