c02bed2dd30e94d205b2d5174eb35d0f5d9a4bd2
Dave Peck <code@davepeck.org>
2009-11-17 13:36:25 -0800

    Clean up URL file issues

--- opentransit/urls.py
+++ opentransit/urls.py
@@ -34,11 +34,10 @@ urlpatterns += patterns(
     url(r'^agency/(.*)/$', 'agency'),
     url(r'^agencies/$', 'agencies'),
     url(r'^agencies/search/$', 'agencies_search'),
-    url('^agencies/(?P<country>[-\w ]+)/$', 'agencies'),
-    url('^agencies/(?P<country>[-\w ]+)/(?P<state>[-\w ]+)/$', 'agencies'),
-    url('^agencies/(?P<country>[-\w ]+)/(?P<state>[-\w ]+)/(?P<city>[-\w ]+)/$', 'agencies'),
-    url('^agencies/(?P<agency>\d+)/$', 'agencies'),
-
+    url(r'^agencies/(?P<country>[\w-]+)/$', 'agencies'),
+    url(r'^agencies/(?P<country>[\w-]+)/(?P<state>[\w-]+)/$', 'agencies'),
+    url(r'^agencies/(?P<country>[\w-]+)/(?P<state>[\w-]+)/(?P<city>[\w-]+)/$', 'agencies'),
+    url(r'^agencies/(?P<agency>\d+)/$', 'agencies'),
 )
 
 
