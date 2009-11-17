cc3dca876d81ab5bbb4acbaeb69f507a350b65f1
Brandon Martin-Anderson <badhill@gmail.com>
2009-11-17 11:03:18 -0800

    loader broken

--- data/agency_loader.py
+++ data/agency_loader.py
@@ -1,15 +1,26 @@
 import datetime
 from google.appengine.ext import db
 from google.appengine.tools import bulkloader
-from ..utils.slug import slugify
+
+import re
+import unicodedata
+
+#from django.utils.encoding import force_unicode
+
+def slugify(value):
+#    value = force_unicode(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub(r'[^\w\s-]', '', value).strip().lower())
+    value = re.sub(r'[-\s]+', '-', value)
+    return value
 
 class AgencyLoader(bulkloader.Loader):
     def __init__(self):
-    
-    def lat_lon(s):
-        lat, lon = [float(v) for v in s.split(',')]
-        return db.GeoPt(lat, lon)
-    
+        
+        def lat_lon(s):
+            lat, lon = [float(v) for v in s.split(',')]
+            return db.GeoPt(lat, lon)
+        
         bulkloader.Loader.__init__(self, 'Agency',
                                    [('long_name', str),
                                     ('short_name', str),
@@ -22,10 +33,9 @@ class AgencyLoader(bulkloader.Loader):
                                     ('location', lat_lon)
                                    ])
                                    
-                                   
     def handle_entity(self, entity):
         entity.update_location()
-        entity.urlslug = '/'.join(map(slugify,[entity.state, entity.city, entity.name)])
+        entity.urlslug = '/'.join(map(slugify,[entity.state, entity.city, entity.name]))
         return entity
 
 loaders = [AgencyLoader]
