8367dcfadf0ab63e90fcd31f4e2c89230474806e
Dave Peck <code@davepeck.org>
2009-11-17 17:08:36 -0800

    IN PROGRESS: The many-to-many relationship between transit applications and agencies.

--- opentransit/models.py
+++ opentransit/models.py
@@ -2,7 +2,8 @@ import logging
 from google.appengine.ext import db
 from django.core.urlresolvers import reverse
 from geo.geomodel import GeoModel
-from utils.slug import slugify
+from .utils.slug import slugify
+from .utils.datastore import normalize_to_keys, unique_entities
 
 class PetitionModel(db.Model):
     name        = db.StringProperty()
@@ -34,6 +35,10 @@ class Agency(GeoModel):
     countryslug     = db.StringProperty()
     urlslug         = db.StringProperty()
     
+    # is_public is True IF at least one of the feeds for this agency (--dave)
+    # TODO bmander: keep this property up to date when you do the feed + agency work.
+    is_public       = db.BooleanProperty(indexed = True)
+    
     def __init__(self, *args, **kwargs):
         # this loads everything to self that's passed as a kwarg, making required and default attribs safe to use
         GeoModel.__init__(self, *args, **kwargs)
@@ -43,7 +48,54 @@ class Agency(GeoModel):
         self.stateslug = slugify(self.state)
         self.countryslug = slugify(self.country)
         self.urlslug = "%s/%s/%s/%s"%(self.countryslug,self.stateslug,self.cityslug,self.nameslug)
-    
+        
+    @staticmethod
+    def all_public_agencies():
+        return Agency.all().filter('is_public =', True)
+        
+    @staticmethod
+    def fetch_for_transit_app(transit_app, uniqify = True):
+        fetched = []
+        if transit_app.supports_all_public_agencies:
+            for public_agency in Agency.all_public_agencies():
+                fetched.append(public_agency)
+        fetched.extend(transit_app.fetch_explicitly_supported_agencies())
+        if uniqify:
+            fetched = unique_entities(fetched)
+        return fetched
+        
+    @staticmethod
+    def iter_for_transit_app(transit_app, uniqify = True):
+        seen = {}
+        if transit_app.supports_all_public_agencies:
+            for public_agency in Agency.all_public_agencies():
+                if (not uniqify) or (public_agency.key() not in seen):
+                    if uniqify: seen[public_agency.key()] = True
+                    yield public_agency
+        for explicit_agency in transit_app.iter_explicitly_supported_agencies():
+            if (not uniqify) or (explicit_agency.key() not in seen):
+                if uniqify: seen[explicit_agency.key()] = True
+                yield explicit_agency
+        
+    @staticmethod
+    def fetch_for_transit_apps(transit_apps, uniqify = True):
+        fetched = []
+        for transit_app in transit_apps:
+            fetched.extend(Agency.fetch_for_transit_app(transit_app, uniqify = False))
+        if uniqify:
+            fetched = unique_entities(fetched)
+        return fetched   
+
+    @staticmethod
+    def iter_for_transit_apps(transit_apps, uniqify = True):
+        seen = {}
+        for transit_app in transit_apps:
+            for agency in Agency.iter_for_transit_app(transit_app, uniqify = False):
+                if (not uniqify) or (agency not in seen):
+                    if uniqify: seen[agency.key()] = True
+                    yield agency        
+
+        
 class FeedReference(db.Model):
     """feed reference models a GTFS Data Exchange entity"""
     
@@ -120,3 +172,58 @@ class TransitApp(db.Model):
     @staticmethod
     def has_transit_app_for_slug(transit_app_slug):
         return (TransitApp.transit_app_for_slug(transit_app_slug) is not None)
+        
+        
+    #
+    # A brief explanation of the way this works:
+    # We basically have two 'kinds' of connection from a transit application to an agency.
+    #
+    # First, transit apps can support "all public agencies." As our list of public agencies grows,
+    # these apps should automatically reflect this. Therefore, we keep a boolean value around.
+    #
+    # At the same time, apps can explicitly support non-public agencies. Here we have a many-many
+    # relationship. Because the typical transit app probably won't have support for a large number
+    # of non-public agencies, we don't have a separate "join" model. Instead we keep a (presumed
+    # small) list of agency keys in each transit app.
+    #
+    # There are corresponding APIs to get all transit applications for a given agency, too.
+    #
+    # With all of these APIs, you must call put() afterward.
+    #
+    # -Dave
+    
+    supports_all_public_agencies = db.BooleanProperty(indexed = True)
+    explicitly_supported_agency_keys = db.ListProperty(db.Key)
+
+    @staticmethod
+    def all_supporting_public_agencies():
+        
+        pass
+        
+    @staticmethod
+    def fetch_for_agency(agency):
+        pass
+        
+    @staticmethod
+    def fetch_for_agencies(agencies):
+        pass
+
+    @property
+    def supported_agencies(self):
+        pass # TODO davepeck
+
+    def fetch_explicitly_supported_agencies(self):
+        return Agency.get(self.explicitly_supported_agency_keys)
+    
+    def add_explicitly_supported_agencies(self, agencies_or_keys):
+        agency_keys = normalize_to_keys(agencies_or_keys)
+        self.explicitly_supported_agency_keys.extend(agency_keys)
+        
+    def add_explicitly_supported_agency(self, angency_or_key):
+        self.add_explicitly_supported_agencies([agency_or_key])
+        
+    def remove_explicitly_supported_agencies(self, agencies_or_keys):
+        agency_keys = normalize_to_keys(agencies_or_keys)
+        for agency_key in agency_keys:
+            self.explicitly_supported_agency_keys.remove(agency_key)
+        
---
+++ opentransit/utils/datastore.py
@@ -0,0 +1,75 @@
+from google.appengine.ext import db
+
+def normalize_to_keys(entities_or_keys):
+    return [eok.key() if isinstance(eok, db.Model) else eok for eok in entities_or_keys]
+
+def unique_keys(keys):
+    unique = {}
+    for key in keys:
+        unique[key] = True
+    return unique.values()
+    
+def unique_entities(entities):
+    unique = {}
+    for entity in entities:
+        unique[entity.key()] = entity
+    return unique.values()
+
+class QueryException(Exception):
+    pass
+
+class AppendedQuery(object):
+    """
+    AppendedQuery allows you to "append" the results of multiple distinct AppEngine
+    queries into a single large query. The primary advantage is that it keeps queries
+    unevaluated until absolutely necessary. It must be used cautiously, because it can be
+    computationally expensive. It is the "end" of a query chain and does not support
+    filter(), order(), or ancestor().
+    
+    It is most efficient to iterate over an AppendedQuery.
+    """
+    def __init__(self, queries):
+        self._queries = queries
+
+    def filter(self, *args, **kwargs):
+        raise QueryException("AppendedQuery does not support filter().")
+        
+    def order(self, *args, **kwargs):
+        raise QueryException("AppendedQuery does not support order().")
+        
+    def ancestor(self, *args, **kwargs):
+        raise QueryException("AppendedQuery does not support ancestor().")
+        
+    def get(self):
+        for query in self._queries:
+            got = query.get()
+            if got is not None:
+                return got
+        return None
+        
+    def fetch(self, limit, offset = None):
+        if offset is not None:
+            raise QueryException("AppendedQuery does not support fetch() with an offset.")            
+        fetched = []
+        for query in self._queries:
+            query_fetched = query.fetch(limit)
+            fetched.extend(query_fetched)
+            limit -= len(query_fetched)
+            if limit <= 0:
+                return fetched
+        return fetched
+    
+    def count(self, limit = 1000):
+        count = 0
+        for query in self._queries:
+            count += query.count(remaining_limit)
+            limit -= count
+            if limit <= 0:
+                return count
+        return count
+        
+    def __iter__(self):
+        for query in self._queries:
+            for entity in query:
+                yield entity
+    
