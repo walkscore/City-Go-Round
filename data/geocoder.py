7010418260756aada5badb38b53d187fd0e0f669
jlivni <jlivni@ucs1.umbrellaconsulting.com>
2009-11-18 16:12:54 -0800

    remove obvious spaces in url

--- data/geocoder.py
+++ data/geocoder.py
@@ -11,8 +11,15 @@ reader =  csv.reader(open('agencies.csv'))
 cols = reader.next()
 print cols
 for r in reader:
-    loc = ', '.join(r[2:3])
-    geo = urllib2.urlopen(geocoder_url % loc).read()
+    loc = r[2].replace(' ','%20') + ',%20' +  r[3]
+    url = geocoder_url % loc
+    try:
+       geo = urllib2.urlopen(url).read()
+    except:
+       print 'error with %s' % url
+       time.sleep(5)
+       geo = urllib2.urlopen(geocoder_url % loc).read()
+
     j = json.loads(geo)
     coords = j['Placemark'][0]['Point']['coordinates']
     r[9] = str(coords[0]) + ',' + str(coords[1])
