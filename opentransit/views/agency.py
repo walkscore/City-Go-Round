20cd57e8e4bb9b956989668794d02992edd79e6f
Dave Peck <code@davepeck.org>
2009-11-20 15:01:45 -0800

    Clean up agency views imports.

--- opentransit/views/agency.py
+++ opentransit/views/agency.py
@@ -1,25 +1,17 @@
 import time
 import logging
+
+from django.http import HttpResponse, HttpResponseRedirect
+
 from google.appengine.ext import db
 from google.appengine.api import memcache
 
 from ..forms import AgencyForm
-from ..utils.view import render_to_response, redirect_to, not_implemented, bad_request, render_to_json
 from ..models import Agency, FeedReference, TransitApp
-
-from django.http import HttpResponse, HttpResponseRedirect
-from ..utils.slug import slugify
+from ..utils.view import render_to_response, redirect_to, not_implemented, bad_request, render_to_json
 
 from StringIO import StringIO
 import csv
-
-
-def uniquify(seq): 
-    # not order preserving 
-    set = {} 
-    map(set.__setitem__, seq, []) 
-    return set.keys()
-
 
 def edit_agency(request, agency_id):
     agency = Agency.get_by_id( int(agency_id) )
