593da9f5691706d72bda3cd35c51e2695bb08c56
Dave Peck <code@davepeck.org>
2009-12-09 18:37:36 -0800

    Final LICENSE file additions and fixes for our code base. Make sure we attribute libraries (and borrowed code) to the appropriate authors.

--- citygoround/utils/securedictionary.py
+++ citygoround/utils/securedictionary.py
@@ -1,10 +1,19 @@
-import os
-
 # NOTE davepeck:
+#
 # This code is a heavily modified version of Werkzeug's secure cookie code.
 # The copyright to this code is owned by Werkzeug's specific authors, though I have
 # made substantial changes. It is no longer concerned just with cookies, and it plays
 # nicely in a django + App Engine world.
+
+# Copyright (c) 2009 by the Werkzeug Team, see http://werkzeug.pocoo.org/license for more details.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# The names of the contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
 
 from hashlib import sha1
 import cPickle as pickle
