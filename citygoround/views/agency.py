af886807805e982745c2a48b3163d87f2ba4c26d
Brandon Martin-Anderson <badhill@gmail.com>
2009-12-15 15:43:36 -0800

    return csv as text/csv

--- citygoround/views/agency.py
+++ citygoround/views/agency.py
@@ -137,9 +137,9 @@ def agencies(request, countryslug='', stateslug='', cityslug='', nameslug=''):
             for item in jsonable_list:
                 csv_writer.writerow( [item[header_col] for header_col in header] )
                         
-            return HttpResponse( content=csv_buffer.getvalue(), mimetype="text/plain" )
+            return HttpResponse( content=csv_buffer.getvalue(), mimetype="text/csv" )
         else:
-            return HttpResponse("")
+            return HttpResponse( content="", mimetype="text/csv")
 
     public_filter = request.GET.get('public','all')
     public_count = no_public_count = 0
